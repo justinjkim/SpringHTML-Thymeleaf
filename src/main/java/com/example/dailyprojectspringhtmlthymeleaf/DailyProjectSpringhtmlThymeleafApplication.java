7001823e01ba55abd6b62f551b468c1016869de9
Justin Kim <jjkim928@gmail.com>
2017-09-26 15:48:24 -0400

    ?

---
+++ src/main/java/com/example/dailyprojectspringhtmlthymeleaf/DailyProjectSpringhtmlThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.example.dailyprojectspringhtmlthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyProjectSpringhtmlThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyProjectSpringhtmlThymeleafApplication.class, args);
+	}
+}
